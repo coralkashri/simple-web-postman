a7e5e0afc650a0100be0999cae4042d341d19a71
Korel Kashri <korelkashri@gmail.com>
2020-05-16 04:37:49 +0300

    Fix fa fonts link. Add index side-navbar. Add admin panel page.

--- js/init.js
+++ js/init.js
@@ -25,8 +25,125 @@
         });
     }
 
+    function init_materialize() {
+        $('select').formSelect();
+        let collapsible = $('.collapsible');
+        collapsible.collapsible();
+        collapsible.filter('.expandable').collapsible({
+            accordion: false
+        });
+        $('.dropdown-trigger').dropdown();
+        $('#user_options_dropdown_trigger').dropdown({
+            //       width: 140,
+            position: "static"
+        });
+        /*$('#advanced_search_version_modal .tabs').tabs({
+            onShow: () => {
+                $('#advanced_search_version_modal .tabs').tabs("select", "advanced_search_by_version");
+            }
+        });*/
+    }
+
+    /**
+     * Initialize Alertify Properties
+     */
+    function initialize_alertify_properties() {
+        alertify.setPosition(false, 1);
+        alertify.setTemplate(function (input) {
+            return "<b>" + input + "</b>";
+        });
+    }
+
+    function init_scroll() {
+        if (!document.getElementById("page_sticky_header")) return;
+        let header, sticky;
+        header = $(".sticky-action");
+        //sticky = header.offsetTop;
+
+        function downAction() {
+            header.removeClass('open');
+            header.removeClass('home');
+            header.addClass('collapse');
+            $(".dropdown-trigger").dropdown('close');
+        }
+
+        function upAction() {
+            header.removeClass('collapse');
+            header.removeClass('home');
+            header.addClass('open');
+            header.addClass("sticky");
+        }
+
+        function homeAction() {
+            header.removeClass('collapse');
+            header.addClass('home');
+            header.addClass('open');
+            header.removeClass("sticky");
+        }
+
+        var scrollTop = function() {
+            return window.scrollY;
+        };
+        let scrollState = 0;
+        let scrollDetect = function(home, down, up) {
+            // Current scroll position
+            var currentScroll = scrollTop();
+            if (scrollTop() === 0) {
+                home();
+            } else if (currentScroll > scrollState) {
+                down();
+            } else {
+                up();
+            }
+            // Set previous scroll position
+            scrollState = scrollTop();
+        };
+        window.onscroll = function () {
+            /*if (window.pageYOffset > sticky) {
+                header_classes.add("sticky");
+            } else {
+                header_classes.remove("sticky");
+            }*/
+            scrollDetect(homeAction, downAction, upAction);
+        };
+    }
+
+    function scroll_to_top() {
+        $("html, body").animate({ scrollTop: $('body').offset().top }, 1000);
+    }
+
+    function init_sidenav() {
+        let sidenav_instance = $(".sidenav");
+        if (sidenav_instance) {
+            sidenav_instance.sidenav({
+                onOpenStart: function () {
+                    $(".dismiss_area").addClass("on");
+                },
+                draggable: true,
+                onCloseEnd: function () {
+                    $(".dismiss_area").removeClass("on");
+                },
+                edge: 'left'
+            });
+        }
+    }
+
+    function init_dismiss_area() {
+        $(".dismiss_area").click(function() {
+            if ($(this).hasClass("on")) {
+                $(".sidenav").sidenav("close");
+                $(".dismiss_area").removeClass("on");
+            }
+        });
+    }
+
     function init_all() {
         initialize_ace();
+        init_materialize();
+        initialize_alertify_properties();
+        init_scroll();
+        init_dismiss_area();
+        init_sidenav();
     }
 
     $(document).ready(function(){
